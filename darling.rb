3b8e917976253c525cfd8bd2a5230b899a664f9e
Nathan Demick <ndemick@covermymeds.com>
2014-01-17 15:26:46 -0500

    Move HTML into its own directory

--- darling.rb
+++ darling.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require 'nokogiri'
 
 Nokogiri::HTML(open('http://www.1101.com/home.html')).css('#todays_darling').each do |content|
-  File.open("#{Time.now.strftime('%Y-%m-%d')}.html", 'w:UTF-8') do |file|
+  File.open("html/#{Time.now.strftime('%Y-%m-%d')}.html", 'w:UTF-8') do |file|
     file << content.to_s
   end
 end
