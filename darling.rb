cc79cba2df1e2a84022d5a513dd0bd1eecc2c850
endemic <ndemick@gmail.com>
2016-05-17 12:07:40 -0400

    Specify UTF-8 when writing the file

--- darling.rb
+++ darling.rb
@@ -1,10 +1,14 @@
-#!/usr/bin/env ruby
+#!/usr/local/bin/ruby
 
 require 'open-uri'
 require 'nokogiri'
 
 Nokogiri::HTML(open('http://www.1101.com/home.html')).css('#todays_darling').each do |content|
-  File.open("html/#{Time.now.strftime('%Y-%m-%d')}.html", 'w:UTF-8') do |file|
+  path = "html/#{Time.now.strftime('%Y-%m-%d')}.html"
+
+  File.open(path, 'w:UTF-8') do |file|
     file << content.to_s
   end
+
+  puts "Wrote content to #{path}"
 end
