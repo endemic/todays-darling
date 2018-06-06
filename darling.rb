d611475f1be19c89c007507e777847be68b0f6e2
endemic <ndemick@gmail.com>
2018-06-06 06:40:45 -0400

    site source HTML was updated
    I didn't notice for like a year, lol

--- darling.rb
+++ darling.rb
@@ -3,7 +3,7 @@
 require 'open-uri'
 require 'nokogiri'
 
-Nokogiri::HTML(open('http://www.1101.com/home.html')).css('#todays_darling').each do |content|
+Nokogiri::HTML(open('http://www.1101.com/home.html')).css('.darling__txt').each do |content|
   path = "html/#{Time.now.strftime('%Y-%m-%d')}.html"
 
   File.open(path, 'w:UTF-8') do |file|
